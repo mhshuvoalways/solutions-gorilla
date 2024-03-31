028de90d0260849c346e11aa42064cd12da14290
MH Shuvo <mhshuvoalways@gmail.com>
2024-03-31 10:54:46 +0600

    fix: make the header z-50

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -4,7 +4,7 @@ const Header = () => {
   const items = ["Our Process", "The Platform", "Pricing", "About Us", ""];
 
   return (
-    <header className="flex items-center justify-between gap-5 py-4 sticky top-0 z-10 backdrop-blur-sm">
+    <header className="flex items-center justify-between gap-5 py-4 sticky top-0 z-50 backdrop-blur-sm">
       <img src="/images/logo.svg" className="w-72 cursor-pointer" />
       <ul className="items-center justify-between gap-4 font-medium text-lg hidden lg:flex">
         <li className="cursor-pointer hover:bg-gray-700 py-1 px-4 rounded-full">

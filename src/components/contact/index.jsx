ed0fd46b1dc2215e6619a054feccd41bb2d769b1
MH Shuvo <mhshuvoalways@gmail.com>
2024-04-10 18:02:18 +0600

    refactor: refactor for localization

--- src/components/contact/index.jsx
+++ src/components/contact/index.jsx
@@ -12,6 +12,8 @@ const Contact = () => {
     reason: "",
     message: "",
   });
+  const [clicked, setClicked] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
 
   const onChange = (event) => {
     const { name, value } = event.target;
@@ -19,9 +21,12 @@ const Contact = () => {
       ...prevState,
       [name]: value,
     }));
+    setClicked(false);
+    setErrorMessage("");
   };
 
   const onSubmit = async (event) => {
+    event.preventDefault();
     if (
       state.name &&
       state.company &&
@@ -29,19 +34,43 @@ const Contact = () => {
       state.reason &&
       state.message
     ) {
-      event.preventDefault();
-      const response = await fetch(
-        "https://n8n.oracle.alebox.xyz/webhook/3987f8bf-7592-4359-9337-dabd2fc43d83",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify([{ body: state }]),
+      setClicked(true);
+      setErrorMessage("");
+      try {
+        const response = await fetch(
+          "https://n8n.oracle.alebox.xyz/webhook/3987f8bf-7592-4359-9337-dabd2fc43d83/",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization:
+                "Basic bGFuZGluZzoqI1h1V0Ejd2ZQU0VnXjV0dG52NF41c0Q=",
+            },
+            body: JSON.stringify(state),
+          }
+        );
+        if (response.ok) {
+          setErrorMessage("Thank you! Your submission has been received!");
+          setState({
+            name: "",
+            company: "",
+            email: "",
+            reason: "",
+            message: "",
+          });
+          setClicked(false);
+        } else {
+          setErrorMessage(
+            "Oops! Something went wrong while submitting the form."
+          );
+          setClicked(false);
         }
-      );
-      const data = await response.json();
-      console.log(data);
+      } catch (error) {
+        setErrorMessage(
+          "Oops! Something went wrong while submitting the form."
+        );
+        setClicked(false);
+      }
     }
   };
 
@@ -100,6 +129,7 @@ const Contact = () => {
             onChange={onChange}
             value={state.reason}
           >
+            <option value={""}>{contact.form.reason.default}</option>
             {contact.form.reason.options.map((op, index) => (
               <option className="bg-rich-black" key={index}>
                 {op}
@@ -114,7 +144,23 @@ const Contact = () => {
             onChange={onChange}
             value={state.message}
           />
-          <button className="bg-white rounded-full w-full p-2 text-black font-bold text-lg hover:bg-sheer-white hover:text-white transition-all">
+          <p
+            className={`text-lg font-bold ${
+              errorMessage
+                ? errorMessage.includes("Oops!")
+                  ? "opacity-100 text-red-600"
+                  : "opacity-100 text-green-600"
+                : "opacity-0"
+            }`}
+          >
+            {errorMessage}
+          </p>
+          <button
+            className={`bg-white rounded-full w-full p-2 text-black font-bold text-lg hover:bg-sheer-white hover:text-white transition-all ${
+              clicked && "opacity-20 cursor-not-allowed"
+            }`}
+            disabled={clicked}
+          >
             {contact.form.btn}
           </button>
         </form>
